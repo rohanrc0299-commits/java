3944b8df1d9b8eda5cd692b492c4aa7e56f8b537
rohanrc0299-commits <rohanrc0299@gmail.com>
2025-11-13 13:27:28 +0530

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.next.int();
+        int b = sc.next.int();
+        int c = a+b;
+
+        System.out.println("sum is" +c);
+    }
+}
